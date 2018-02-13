41c301cb5a478803d5f6cca475107b24a490df8b
Unknown <monika.silvia.s@hotmail.ca>
2018-02-12 21:18:31 -0800

    adjustments to logo startpagetest

--- startpagetest.php
+++ startpagetest.php
@@ -29,12 +29,12 @@
                   <img id="LogoTopStopper" src="Images/LogoParts/TopStopper.svg" alt="TopStopper" height="75" width="75">
                   <img id="LeftStopper" src="Images/LogoParts/LeftStopper.svg" alt="LeftStopper" height="50" width="50">
                   <img id="RightStopper" src="Images/LogoParts/RightStopper.svg" alt="RightStopper" height="50" width="50">
-                  <img id="LogoArmShort" src="Images/LogoParts/LogoArm.svg" alt="LogoArm" height="75" width="60">
-                  <img id="LogoArmLong" src="Images/LogoParts/LogoArm2.svg" alt="LogoArmLong" height="75" width="100">
-                  <img id="Head" src="Images/LogoParts/Head.svg" alt="Head" height="75" width="75">
-                  <img id="TopBody" src="Images/LogoParts/TopBody.svg" alt="TopBody" height="75" width="75">
-                  <img id="RightLeg" src="Images/LogoParts/RightLeg.svg" alt="RightLeg" height="75" width="75">
-                  <img id="LeftLeg" src="Images/LogoParts/LeftLeg.svg" alt="LeftLeg" height="75" width="75">
+                  <img id="LogoArmShort" src="Images/LogoParts/LogoArm.svg" alt="LogoArm" height="50" width="50">
+                  <img id="LogoArmLong" src="Images/LogoParts/LogoArm2.svg" alt="LogoArmLong" height="75" width="80">
+                  <img id="Head" src="Images/LogoParts/Head.svg" alt="Head" height="60" width="60">
+                  <img id="TopBody" src="Images/LogoParts/TopBody.svg" alt="TopBody" height="60" width="60">
+                  <img id="RightLeg" src="Images/LogoParts/RightLeg.svg" alt="RightLeg" height="55" width="55">
+                  <img id="LeftLeg" src="Images/LogoParts/LeftLeg.svg" alt="LeftLeg" height="55" width="55">
                 </div>
 
               </div>
