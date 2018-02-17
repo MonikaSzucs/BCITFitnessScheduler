7f2c3f98688da842b22cc1b559148dc2f9517486
Daren Capacio <megamanv5@hotmail.com>
2018-02-16 16:28:01 -0800

    Added additional sources to allow our app to use the User's calender info

--- src/config.php
+++ src/config.php
@@ -71,6 +71,8 @@ $apiConfig = array(
           'scope' => array(
               'https://www.googleapis.com/auth/userinfo.profile',
               'https://www.googleapis.com/auth/userinfo.email',
+              'https://www.googleapis.com/auth/calendar',
+              'https://www.googleapis.com/auth/calendar.readonly'
           )
       ),
       'plus' => array('scope' => 'https://www.googleapis.com/auth/plus.me'),
