2e7a341fd04083b859343244a40f048f167997db
Unknown <monika.silvia.s@hotmail.ca>
2018-03-01 23:11:57 -0800

    aniamted arms on logo index and merged my animation with the primary index.php file

--- index.php
+++ index.php
@@ -29,13 +29,13 @@ if ($gClient->getAccessToken()) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/Main.css">
+    <link rel="stylesheet" href="css/LogoCSS.css">
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/logo-animation.js"></script>
   </head>
 
-  <body onload="animationStart()">
+  <body >
 
     <div class="vertical-center">
       <div class="container text-center">
@@ -50,13 +50,21 @@ if ($gClient->getAccessToken()) {
               <div class="col-md-offset-3 col-md-6">
 
                 <div id="logoArea">
+
+									<img id="LeftWB" src="Icons/LogoParts/LeftWB.svg" alt="LeftWB" height="230" width="230">
+									<img id="RightWB" src="Icons/LogoParts/RightWB.svg" alt="LeftWB" height="230" width="230">
+
                   <div id="LogoOuterRing"></div>
                   <div id="LogoTopStopper"></div>
                   <img id="LeftStopper" src="Icons/LogoParts/LeftStopper.svg" alt="LeftStopper" height="50" width="50">
                   <img id="RightStopper" src="Icons/LogoParts/RightStopper.svg" alt="RightStopper" height="50" width="50">
+                  <img id="OuterRingAll" src="Icons/LogoParts/OuterTimer.svg" alt="LogoArm" height="300" width="300">
+
+
                   <img id="LogoArmShort" src="Icons/LogoParts/LogoArm.svg" alt="LogoArm" height="50" width="50">
-                  <img id="LogoArmLong" src="Icons/LogoParts/LogoArm2.svg" alt="LogoArmLong" height="75" width="80">
-                  <img id="Head" src="Icons/LogoParts/Head.svg" alt="Head" height="60" width="60">
+                  <img id="LogoArmLong1" src="Icons/LogoParts/LogoArm2.svg" alt="LogoArmLong" height="75" width="80">
+
+									<img id="Head" src="Icons/LogoParts/Head.svg" alt="Head" height="60" width="60">
                   <img id="TopBody" src="Icons/LogoParts/TopBody.svg" alt="TopBody" height="60" width="60">
                   <img id="RightLeg" src="Icons/LogoParts/RightLeg.svg" alt="RightLeg" height="55" width="55">
                   <img id="LeftLeg" src="Icons/LogoParts/LeftLeg.svg" alt="LeftLeg" height="55" width="55">
